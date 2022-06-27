252304a902c394e5e1a990b4e35ba9ab6507c054
Bruna Reveriego <Bruna.Reveriego@br.ey.com>
2022-06-27 20:26:12 -0300

    csv to array converter

--- ConvertData.Domain/GenerateService.cs
+++ ConvertData.Domain/GenerateService.cs
@@ -1,7 +1,5 @@
-﻿using ClosedXML.Excel;
-using ConvertData.Domain.Interfaces;
+﻿using ConvertData.Domain.Interfaces;
 using ConvertData.Domain.Response;
-using ExcelDataReader;
 using Microsoft.AspNetCore.Http;
 
 namespace ConvertData.Domain
@@ -11,98 +9,161 @@ namespace ConvertData.Domain
         public async Task<StateResponse> FileSplitWriter(IFormFile _file)
         {
             System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
-            using (var stream = new MemoryStream())
+
+            var stream = new MemoryStream();
+
+            _file.CopyTo(stream);
+
+
+            stream.Position = 0;
+
+            var ws = new StreamReader(stream);
+            string header = "";
+
+            int rows = 0;
+            string currentLine;
+            while ((currentLine = ws.ReadLine()) != null)
             {
-                _file.CopyTo(stream);
-                stream.Position = 0;
+                if (rows == 0)
+                    header = currentLine;
 
-                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                rows++;
+            }
+
+            stream.Position = 0;
+
+            int columns = ws.ReadLine().Split(';').Count();
+            int rowCount = 0;
+            int totalOfRows = 2500;
+            int columnCount = 0;
+
+            string[,] array = new string[rows, columns];
+            string[] headers = header.Split(';');
+
+            //ws.Close();
+
+
+            stream.Position = 0;
+
+
+
+            while (!ws.EndOfStream) //Each row of the file
+            {
+                var line = ws.ReadLine().ToString();
+                var values = line.Split(';');
+
+
+                foreach (var item in values)
                 {
-                    int rowCount = 0;
-                    int totalOfRows = 2500;
-                    int columnCount = 0;
-                    string[,] array = new string[reader.RowCount , reader.FieldCount];
-                    string[,] headers = new string[2, reader.FieldCount];
-
-                    while (reader.Read()) //Each row of the file
+                    if (rowCount == 0)
                     {
-
-                        while (columnCount < reader.FieldCount)
+                        for (var headItem = 0; headItem < headers.Length; headItem++)
                         {
-                            if (rowCount < 1)
-                            {
-                                headers[rowCount, columnCount] = reader.GetValue(columnCount) == null ? "" : reader.GetValue(columnCount).ToString();
-                            }
-
-                            array[rowCount, columnCount] = reader.GetValue(columnCount) == null ? "" : reader.GetValue(columnCount).ToString();
-                            columnCount++;
+                            array[rowCount, headItem] = headers[headItem].ToString() == null ? "" : headers[headItem].ToString();
                         }
-
-                        rowCount++;
-                        columnCount = 0;
-
-                    }
-
-                    double totalOfFiles = Math.Ceiling(((float)reader.RowCount - 1) / totalOfRows);
-                    var fileNumber = 1;
-
-                    var start = 0;
-                    var end = 1;
-
-                    while (fileNumber <= totalOfFiles)
+                    } else
                     {
-                        var file = @"C:\myOutput" + fileNumber + ".csv";
-
-                        start = end;
-                        if (fileNumber == totalOfFiles)
-                        {
-                            var rowsLastFile = reader.RowCount - end;
-                            end += rowsLastFile;
-                        }
-                        else
-                        {
-                            
-                            end = end + totalOfRows;
-                        }
-
-                        //generate xlxs files
-                        var workbook = new XLWorkbook();
-                        workbook.AddWorksheet("sheetName");
-                        var ws = workbook.Worksheet("sheetName");
-
-
-
-                        for (var x = 1; x < 2; x++)
-                        {
-                            for (var y = 1; y < reader.FieldCount; y++)
-                            {
-                                ws.Cell(x, y).Value = headers[x - 1, y - 1].ToString();
-                            }
-
-                        }
-
-
-                        var row = 2;
-
-                        for (var x = start; x <= end; x++)
-                        {
-                            for (var y = 1; y < reader.FieldCount; y++)
-                            {
-                                ws.Cell(row, y).Value = array[x - 1, y - 1].ToString();
-                            }
-                            row++;
-                        }
-
-                        workbook.SaveAs("yourExcel" + "-" + fileNumber + ".csv");
-                        fileNumber++;
-
+                        array[rowCount, columnCount] = item.ToString() == null ? "" : item.ToString();
                     }
+                    
+                    columnCount++;
 
-                    return new StateResponse(System.Net.HttpStatusCode.OK, "Generated Files");
                 }
+
+
+                rowCount++;
+                columnCount = 0;
+
             }
 
 
+
+
+
+
+            //double totalOfFiles = Math.Ceiling(((float)reader.RowCount - 1) / totalOfRows);
+            //var fileNumber = 1;
+
+            //var start = 0;
+            //var end = 1;
+
+            //while (fileNumber <= totalOfFiles)
+            //{
+            //    var file = @"C:\myOutput" + fileNumber + ".csv";
+
+            //    start = end;
+            //    if (fileNumber == totalOfFiles)
+            //    {
+            //        var rowsLastFile = reader.RowCount - end;
+            //        end += rowsLastFile;
+            //    }
+            //    else
+            //    {
+
+            //        end = end + totalOfRows;
+            //    }
+
+            //    //generate xlxs files
+            //    var workbook = new XLWorkbook();
+            //    workbook.AddWorksheet("sheetName");
+            //    var ws = workbook.Worksheet("sheetName");
+
+
+
+            //    for (var x = 1; x < 2; x++)
+            //    {
+            //        for (var y = 1; y < reader.FieldCount; y++)
+            //        {
+            //            ws.Cell(x, y).Value = headers[x - 1, y - 1].ToString();
+            //        }
+
+            //    }
+
+
+            //    var row = 2;
+
+            //    for (var x = start; x <= end; x++)
+            //    {
+            //        for (var y = 1; y < reader.FieldCount; y++)
+            //        {
+            //            ws.Cell(row, y).Value = array[x - 1, y - 1].ToString();
+            //        }
+            //        row++;
+            //    }
+
+            //    workbook.SaveAs("yourExcel" + "-" + fileNumber + ".csv");
+            //    fileNumber++;
+
+            //}
+
+            return new StateResponse(System.Net.HttpStatusCode.OK, "Generated Files");
+
+
+
+
+        }
+
+
+        private int CountRowsFile(IFormFile file)
+        {
+            using (var str = new MemoryStream())
+            {
+                int rows = 1;
+                file.CopyTo(str);
+
+                using (var rdr = new StreamReader(str))
+                {
+                    while (rdr.ReadLine() != null)
+                    {
+                        rows++;
+                    }
+
+
+                    str.Position = 0;  //Site tip
+                    rdr.Dispose();
+                    return rows;
+                }
+            }
         }
     }
 }
